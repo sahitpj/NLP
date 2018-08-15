bbd01a11ede5a2078acbce53ecb1a42e379549fe
Jayakrishna Sahit <jayakrishna.sahit@iitgn.ac.in>
2018-08-15 15:18:30 +0530

    Added Ipython changes to .py files

--- assignments/assignment1/zipfLaw.py
+++ assignments/assignment1/zipfLaw.py
@@ -4,19 +4,18 @@ import matplotlib.pyplot as plt
 import random
 import math
 
-print math.log(10)
 
 textInWords = loader.loader("dataCollection")[1]
 Ranks = loader.loader("dataCollection")[2]
 wordFrequencies = loader.loader("dataCollection")[3]
 
-randints = [random.randint(0, 8000) for i in xrange(10)]
-testWords = sorted([ textInWords[randints[i]] for i in xrange(10)])
-wordRanks = [ Ranks[randints[i]] for i in xrange(10)]
+randints = [random.randint(0, 8000) for i in xrange(30)]
+testWords = sorted([ textInWords[randints[i]] for i in xrange(30)])
+wordRanks = [ Ranks[randints[i]] for i in xrange(30)]
 
-noOfWordMeanings = [ len(wn.synsets(testWords[i])) for i in xrange(10)]
-wordLength = [ len(testWords[i]) for i in xrange(10)]
-wordFrequencies = [ wordFrequencies[randints[i]] for i in xrange(10)]
+noOfWordMeanings = [ len(wn.synsets(testWords[i])) for i in xrange(30)]
+wordLength = [ len(testWords[i]) for i in xrange(30)]
+wordFrequencies = [ wordFrequencies[randints[i]] for i in xrange(30)]
 
 logOfWordRanks = []
 logOfWordFrequencies = []
@@ -25,7 +24,7 @@ logWordMeanings = []
 
 
 
-for i in xrange(10):
+for i in xrange(30):
     if noOfWordMeanings[i] == 0:
         noOfWordMeanings[i] = 1
     logOfWordFrequencies.append(math.log(float(wordRanks[i])))
@@ -35,14 +34,27 @@ for i in xrange(10):
 
 
 
-plt.plot(logWordMeanings,logOfWordRanks, 'r', '-')
-plt.plot(logWordMeanings, logOfWordFrequencies, 'b', '-.-')
-plt.plot(logOfWordLength, logOfWordFrequencies, 'g')
-plt.plot(logOfWordLength, logOfWordRanks, 'c')
+plt.scatter(logOfWordFrequencies, logOfWordRanks)
 plt.show()
 
-lVSf = [ wordFrequencies[i]*wordLength[i] for i in xrange(10)]
-lVSr = [ wordRanks[i]/float(wordLength[i]) for i in xrange(10)]
-mVSf = [ math.sqrt(wordFrequencies[i])/float(noOfWordMeanings[i]) for i in xrange(10)]
-mVSr = [ math.sqrt(wordRanks[i])*float(noOfWordMeanings[i]) for i in xrange(10)]
+plt.scatter(logWordMeanings,logOfWordRanks)
+plt.ylim((3,10))
+plt.show()
+
+plt.scatter(logWordMeanings, logOfWordFrequencies)
+plt.ylim((-10,20))
+plt.show()
+
+plt.scatter(logOfWordLength, logOfWordFrequencies)
+plt.ylim((-10,20))
+plt.show()
+
+plt.scatter(logOfWordLength, logOfWordRanks)
+plt.ylim((0,10))
+plt.show()
+
+lVSf = [ wordFrequencies[i]*wordLength[i] for i in xrange(30)]
+lVSr = [ wordRanks[i]/float(wordLength[i]) for i in xrange(30)]
+mVSf = [ math.sqrt(wordFrequencies[i])/float(noOfWordMeanings[i]) for i in xrange(30)]
+mVSr = [ math.sqrt(wordRanks[i])*float(noOfWordMeanings[i]) for i in xrange(30)]
 
